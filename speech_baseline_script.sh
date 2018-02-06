38a5274697e0aa975100850458450dfb12d9a0cb
rhoposit <s1738075@sms.ed.ac.uk>
2018-02-06 12:16:23 +0000

    setting up for speech baseline

--- speech_baseline_script.sh
+++ speech_baseline_script.sh
@@ -3,14 +3,14 @@
 #SBATCH -n 1	  # tasks requested
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16000  # memory in Mb
-#SBATCH -o sample_experiment_outfile  # send stdout to sample_experiment_outfile
-#SBATCH -e sample_experiment_errfile  # send stderr to sample_experiment_errfile
+#SBATCH -o outfile  # send stdout to sample_experiment_outfile
+#SBATCH -e errfile  # send stderr to sample_experiment_errfile
 #SBATCH -t 2:00:00  # time requested in hour:minute:secon
 export CUDA_HOME=/opt/cuda-8.0.44
 
 export CUDNN_HOME=/opt/cuDNN-6.0_8.0
 
-export STUDENT_ID=sxxxxxx
+export STUDENT_ID=s1738075
 
 export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
 
@@ -30,4 +30,5 @@ export TMP=/disk/scratch/${STUDENT_ID}/
 
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 
-python emnist_network_trainer.py --batch_size 128 --epochs 200 --experiment_prefix vgg-net-emnist-sample-exp --dropout_rate 0.4 --batch_norm_use True --strided_dim_reduction True --seed 25012018
+#python emnist_network_trainer.py --batch_size 128 --epochs 200 --experiment_prefix vgg-net-emnist-sample-exp --dropout_rate 0.4 --batch_norm_use True --strided_dim_reduction True --seed 25012018
+python explore2.py 
