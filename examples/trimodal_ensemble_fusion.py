47c0d6c4efd2823b9d63bc4be7275dfb56f59f31
comRamona <com.ramona@yahoo.com>
2018-02-18 18:17:40 +0000

    trimodal improvements and plotting with keras

--- examples/trimodal_ensemble_fusion.py
+++ examples/trimodal_ensemble_fusion.py
@@ -21,6 +21,7 @@ from mmdata import MOSI
 import argparse
 from collections import defaultdict
 from mmdata.dataset import Dataset
+import time
 
 from utils.parser_utils import KerasParserClass
 from utils.storage import build_experiment_folder, save_statistics
@@ -48,44 +49,54 @@ from keras.optimizers import Adam
 from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional, BatchNormalization
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 
+
 parser = argparse.ArgumentParser(description='Welcome to LSTM experiments script')  # generates an argument parser
 parser_extractor = KerasParserClass(parser=parser)  # creates a parser class to process the parsed input
 
 batch_size, seed, epochs, logs_path, continue_from_epoch, batch_norm, \
 experiment_prefix, dropout_rate, n_layers, max_len = parser_extractor.get_argument_variables()
-
-
+if(experiment_prefix =="classification"):
+    experiment_prefix = "Trimodal" + time.strftime("%Y-%m-%d %H:%M")
 experiment_name = "experiment_{}_batch_size_{}_bn_{}_dr{}_nl_{}_ml_{}".format(experiment_prefix,
                                                                    batch_size, batch_norm,
                                                                    dropout_rate, n_layers, max_len)
+saved_models_filepath, logs_filepath = build_experiment_folder(experiment_name, logs_path)
+filepath = "{}/best_validation_{}".format(saved_models_filepath, experiment_name)
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 
 from multimodaldata import get_data
 train_set_audio, valid_set_audio, test_set_audio, train_set_text, valid_set_text, test_set_text, \
 train_set_visual, valid_set_visual, test_set_visual, \
 y_train, y_valid, y_test = get_data(max_len_audio=20, max_len_text=15, max_len_visual=20)
 
+
+filepath = "{}/best_validation_{}_".format(saved_models_filepath, experiment_name)
+weights = "{}_weights{}.h5"
 # pdb.set_trace()
 k=3
 m=2
 # AUDIO
-model1_in = Input(name="Input1",shape=(train_set_audio.shape[1], train_set_audio.shape[2]))
+model1_in = Input(name="Audio_Covarep",shape=(train_set_audio.shape[1], train_set_audio.shape[2]))
 model1_cnn = Conv1D(filters=64, kernel_size=k, activation='relu')(model1_in)
 model1_mp = MaxPooling1D(m)(model1_cnn)
 model1_fl = Flatten()(model1_mp)
 model1_dense = Dense(128, activation="relu", W_regularizer=l2(0.0001))(model1_fl)
-model1_out = Dense(1, name='layer_1')(model1_dense)
+model1_out = Dense(1, name='Sigmoid_Audio')(model1_dense)
 model1 = Model(model1_in, model1_out)
 model1.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model1.summary()
-checkpoint1 = ModelCheckpoint('t_weights1.h5', monitor='val_acc',
+checkpoint1 = ModelCheckpoint(weights.format(filepath,1), monitor='val_acc',
 save_best_only=True, verbose=2, mode="max")
-early_stopping1 = EarlyStopping(monitor="val_loss", patience=15, mode="max")
+early_stopping1 = EarlyStopping(monitor="val_acc", patience=10, mode="max")
 model1.fit(train_set_audio, y=y_train, batch_size=32, epochs=100,
              verbose=1, validation_data=[valid_set_audio, y_valid], shuffle=True, callbacks=[early_stopping1, checkpoint1])
-model1.load_weights("t_weights1.h5")
+model1.load_weights(weights.format(filepath,1))
+preds = model1.predict(test_set_audio)
+acc = np.mean((preds > 0.5) == y_test.reshape(-1, 1))
+print("Audio Test accuracy: ", acc)
 
 # TEXT
-model2_in = Input(name="Input2",shape=(train_set_text.shape[1], train_set_text.shape[2]))
+model2_in = Input(name="Text_GloVe",shape=(train_set_text.shape[1], train_set_text.shape[2]))
 model2_blstm = Bidirectional(LSTM(64))(model2_in)
 model2_d1 = Dropout(dropout_rate)(model2_blstm)
 model2_d2 = Dense(200, activation="relu", W_regularizer=l2(0.0001))(model2_d1)
@@ -93,67 +104,79 @@ model2_d3 = Dropout(dropout_rate)(model2_d2)
 model2_d4 = Dense(200, activation="relu", W_regularizer=l2(0.0001))(model2_d3)
 model2_d5 = Dropout(dropout_rate)(model2_d4)
 model2_d6 = Dense(200, activation="relu", W_regularizer=l2(0.0001))(model2_d5)
-model2_out = Dense(1, name='layer_2')(model2_d6)
+model2_out = Dense(1, name='Sigmoid_Text')(model2_d6)
 model2 = Model(model2_in, model2_out)
 model2.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model2.summary()
-checkpoint2 = ModelCheckpoint('t_weights2.h5', monitor='val_acc',
+checkpoint2 = ModelCheckpoint(weights.format(filepath,2), monitor='val_acc',
 save_best_only=True, verbose=2,mode="max")
-early_stopping2 = EarlyStopping(monitor="val_acc", patience=20,mode="max")
+early_stopping2 = EarlyStopping(monitor="val_acc", patience=10,mode="max")
 model2.fit(train_set_text, y=y_train, batch_size=32, epochs=100,
              verbose=1, validation_data=[valid_set_text, y_valid], shuffle=True, callbacks=[early_stopping2, checkpoint2])
-model2.load_weights("t_weights2.h5")
-score, acc = model2.evaluate(test_set_text, y_test)
+model2.load_weights(weights.format(filepath,2))
+preds = model2.predict(test_set_text)
+acc = np.mean((preds > 0.5) == y_test.reshape(-1, 1))
 print("Text Test accuracy: ", acc)
 
 # VISUAL
-model3_in = Input(name="Input3",shape=(train_set_visual.shape[1], train_set_visual.shape[2]))
+model3_in = Input(name="Video_Facet",shape=(train_set_visual.shape[1], train_set_visual.shape[2]))
 model3_cnn = Conv1D(filters=64, kernel_size=k, activation='relu')(model3_in)
 model3_mp = MaxPooling1D(m)(model3_cnn)
 model3_fl = Flatten()(model3_mp)
-model3_dense = Dense(128, activation="relu", W_regularizer=l2(0.0001))(model3_fl)
-model3_out = Dense(1, name='layer_3')(model3_dense)
+model3_dense = Dense(128, activation="relu", W_regularizer=l2(0.001))(model3_fl)
+model3_out = Dense(1, name='Sigmoid_Video')(model3_dense)
 model3 = Model(model3_in, model3_out)
 model3.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model3.summary()
-checkpoint3 = ModelCheckpoint('t_weights3.h5', monitor='val_acc',
+checkpoint3 = ModelCheckpoint(weights.format(filepath,3), monitor='val_acc',
 save_best_only=True, verbose=2, mode="max")
-early_stopping3 = EarlyStopping(monitor="val_loss", patience=15, mode="max")
+early_stopping3 = EarlyStopping(monitor="val_acc", patience=10, mode="max")
 model3.fit(train_set_visual, y=y_train, batch_size=32, epochs=100,
              verbose=1, validation_data=[valid_set_visual, y_valid], shuffle=True, callbacks=[early_stopping3, checkpoint3])
-model3.load_weights("t_weights3.h5")
-score, acc = model3.evaluate(test_set_visual, y_test)
+model3.load_weights(weights.format(filepath,3))
+preds = model3.predict(test_set_video)
+acc = np.mean((preds > 0.5) == y_test.reshape(-1, 1))
 print("Video Test accuracy: ", acc)
 
 concatenated = concatenate([model1_out, model2_out, model3_out])
-out = Dense(1, activation='sigmoid', name='output_layer')(concatenated)
+out = Dense(1, activation='sigmoid', name='Sigmoid_Output')(concatenated)
 
 merged_model = Model([model1_in, model2_in, model3_in], out)
 merged_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 merged_model.summary()
-checkpoint = ModelCheckpoint('t_weights.h5', monitor='val_acc',
-save_best_only=True, verbose=2)
-early_stopping = EarlyStopping(monitor="val_loss", patience=15)
+
+tensor_board = TensorBoard(log_dir=logs_filepath, histogram_freq=0, batch_size=batch_size, write_graph=True, 
+    write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None)
+checkpoint = ModelCheckpoint(weights.format(filepath, "merged"), monitor='val_acc',
+save_best_only=True, verbose=2, mode="max")
+early_stopping = EarlyStopping(monitor="val_acc", patience=15, mode="max")
 merged_model.fit([train_set_audio, train_set_text, train_set_visual], y=y_train, batch_size=32, epochs=100,
              verbose=1, validation_data=[[valid_set_audio, valid_set_text, valid_set_visual],y_valid], shuffle=True, 
-callbacks=[early_stopping, checkpoint])
-merged_model.load_weights("t_weights.h5")
+callbacks=[early_stopping, checkpoint, tensor_board])
+merged_model.load_weights(weights.format(filepath, "merged"))
 
 score, acc = merged_model.evaluate([test_set_audio, test_set_text, test_set_visual], y_test)
 print("Test accuracy: ", acc)
+print("TensorBoard: ", logs_filepath)
 
 model_json = merged_model.to_json()
-with open("trimodal_model.json", "w") as json_file:
+with open(filepath + "trimodal_model.json", "w") as json_file:
     json_file.write(model_json)
-merged_model.save_weights("trimodal_model.h5")
+merged_model.save_weights(filepath + "trimodal_model.h5")
 print("Saved model to disk")
 
 from keras.models import model_from_json
-json_file = open('trimodal_model.json', 'r')
+json_file = open(filepath + 'trimodal_model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
-loaded_model.load_weights("trimodal_model.h5")
+loaded_model.load_weights(filepath + "trimodal_model.h5")
 print("Loaded model from disk")
 print(loaded_model.layers[-1].get_weights())
+
+from keras.utils import plot_model
+plot_model(merged_model, to_file='merged_model.png')
+plot_model(model1, to_file='model1.png')
+plot_model(model2, to_file='model2.png')
+plot_model(model3, to_file='model3.png')
