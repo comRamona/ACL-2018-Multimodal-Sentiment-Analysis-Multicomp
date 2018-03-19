6afeb57a812669c49c346fba10dcb8f945af4e46
Jennifer Williams <s1738075@pedroso.inf.ed.ac.uk>
2018-03-19 16:01:34 +0000

    lstm experiment code

---
+++ lstm_groups.sh
@@ -0,0 +1,26 @@
+python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 15 --dropout_rate 0.1 --n_layers 2 >> lstm_$1_1.txt 2>&1 &
+python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 20 --dropout_rate 0.1 --n_layers 2 >> lstm_$1_2.txt 2>&1 &
+python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 25 --dropout_rate 0.1 --n_layers 2 >> lstm_$1_3.txt 2>&1 &
+python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 30 --dropout_rate 0.1 --n_layers 2 >> lstm_$1_4.txt 2>&1 &
+python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 15 --dropout_rate 0.2 --n_layers 2 >> lstm_$1_5.txt 2>&1 &
+python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 20 --dropout_rate 0.2 --n_layers 2 >> lstm_$1_6.txt 2>&1 &
+python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 25 --dropout_rate 0.2 --n_layers 2 >> lstm_$1_7.txt 2>&1 &
+python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 30 --dropout_rate 0.2 --n_layers 2 >> lstm_$1_8.txt 2>&1 &
+
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 15 --dropout_rate 0.1 --n_layers 2 >> lstm_$1_9.txt 2>&1 &
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 20 --dropout_rate 0.1 --n_layers 2 >> lstm_$1_10.txt 2>&1 &
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 25 --dropout_rate 0.1 --n_layers 2 >> lstm_$1_11.txt 2>&1 &
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 30 --dropout_rate 0.1 --n_layers 2 >> lstm_$1_12.txt 2>&1 &
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 15 --dropout_rate 0.2 --n_layers 2 >> lstm_$1_13.txt 2>&1 &
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 20 --dropout_rate 0.2 --n_layers 2 >> lstm_$1_14.txt 2>&1 &
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 25 --dropout_rate 0.2 --n_layers 2 >> lstm_$1_15.txt 2>&1 &
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 30 --dropout_rate 0.2 --n_layers 2 >> lstm_$1_16.txt 2>&1 &
+
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 15 --dropout_rate 0.1 --n_layers 3 >> lstm_$1_17.txt 2>&1 &
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 20 --dropout_rate 0.1 --n_layers 3 >> lstm_$1_18.txt 2>&1 &
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 25 --dropout_rate 0.1 --n_layers 3 >> lstm_$1_19.txt 2>&1 & 
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 30 --dropout_rate 0.1 --n_layers 3 >> lstm_$1_20.txt 2>&1 &
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 15 --dropout_rate 0.2 --n_layers 3 >> lstm_$1_21.txt 2>&1 &
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 20 --dropout_rate 0.2 --n_layers 3 >> lstm_$1_22.txt 2>&1 &
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 25 --dropout_rate 0.2 --n_layers 3 >> lstm_$1_23.txt 2>&1 &
+#python early_fusion_lstm.py --experiment_prefix lstm_early_fusion --mode $1 --max_len 30 --dropout_rate 0.2 --n_layers 3 >> lstm_$1_24.txt 2>&1 &
